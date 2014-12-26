0290baa831299cbe199bc12eb79b187a51c93315
wangtaoking1 <479021795@qq.com>
2014-12-26 21:00:43 +0800

    add the login window

--- src/com/wt/main/MailClient.java
+++ src/com/wt/main/MailClient.java
@@ -1,14 +1,10 @@
 package com.wt.main;
 
-import com.wt.utils.MailMessage;
-import com.wt.utils.User;
-import com.wt.smtp.SMTPClient;
-import org.apache.commons.codec.binary.Base64;
+import com.wt.gui.LoginFrame;
 
 public class MailClient {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	    new LoginFrame();
 	}
-
 }
